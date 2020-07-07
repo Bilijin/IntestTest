c89e3bdfc6908ed0cd5890dcf2ad30611843abdc
Alabi <alabibolaji78@gmail.com>
2020-07-07 20:20:41 +0100

    created a new app

---
+++ app/src/main/java/com/example/intenttestdrive/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.intenttestdrive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mLocation, mSendText, mOpenWedsite;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mOpenWedsite = findViewById(R.id.website);
+        OpenWebsite();
+    }
+
+    public void OpenWebsite() {
+        String url = mOpenWedsite.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else {
+            Log.d("openwebsite","can't open website");
+        }
+    }
+}
